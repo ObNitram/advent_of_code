430f75cd51ea60eff6ea676a17cf4b59efa84e71
ObNitram <mart1.guillemot@gmail.com>
2023-12-09 12:40:12 +0800

    day 9 0
    Took 1 hour 16 minutes

--- lib/day_8.dart
+++ lib/day_8.dart
@@ -1,4 +1,5 @@
 import "package:advent_of_code/get_file.dart";
+import "package:obni_utils/obni_utils.dart";
 
 String data = getFile();
 
@@ -16,51 +17,54 @@ void main() {
   List<String> a = dataLines.last.trim().split("\n");
 
   for (String b in a) {
-    RegExp exp = RegExp(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3}|)");
+    RegExp exp = RegExp(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})");
 
     exp.allMatches(b).forEach((element) {
       map[element.group(1)!] = (element.group(2)!, element.group(3)!);
     });
   }
 
-  InfinitIterator<String> iterator = InfinitIterator(directions);
-  String currentPosition = "AAA";
+  List<String> currentPositions = [];
 
-  do {
-    String currentDirection = iterator.current;
-
-    print("$currentPosition $currentDirection");
-
-    if (currentPosition == "ZZZ") {
-      print("result : $result");
-      return;
+  for (var key in map.keys) {
+    if (key.endsWith("A")) {
+      currentPositions.add(key);
     }
-
-    result += 1;
-
-    if (currentDirection == "L") {
-      currentPosition = map[currentPosition]!.$1;
-      continue;
-    }
-    if (currentDirection == "R") {
-      currentPosition = map[currentPosition]!.$2;
-      continue;
-    }
-  } while (iterator.moveNext());
-}
-
-class InfinitIterator<String> implements Iterator<String> {
-  final List<String> _list;
-  int _index = 0;
-
-  InfinitIterator(this._list);
-
-  @override
-  String get current => _list[_index];
-
-  @override
-  bool moveNext() {
-    _index = (_index + 1) % _list.length;
-    return true;
   }
+
+  List<int> rotations = [];
+
+  for (var o in currentPositions) {
+    InfinitIterator<String> iterator = directions.infinitIterator;
+
+    String currentPosition = o;
+
+    //print("Start: $currentPosition");
+
+    int counter = 0;
+    do {
+      String currentDirection = iterator.current;
+
+      if (currentPosition.endsWith("Z")) {
+        rotations.add(counter);
+        break;
+      }
+
+      print("$currentPosition $currentDirection");
+      counter += 1;
+
+      if (currentDirection == "L") {
+        currentPosition = map[currentPosition]!.$1;
+        continue;
+      }
+      if (currentDirection == "R") {
+        currentPosition = map[currentPosition]!.$2;
+        continue;
+      }
+    } while (iterator.moveNext());
+    print("End: $currentPosition");
+  }
+  print(rotations.getPPCM());
 }
+
+// 7309459565207
---
+++ lib/day_9.dart
@@ -0,0 +1,17 @@
+import "package:advent_of_code/get_file.dart";
+
+String data = getFile();
+
+List<String> dataLines = data.split("\n");
+
+int result = 1;
+
+void main() {
+  print("Hello, day 6 !");
+
+  for (int i = 0; i < dataLines.length; ++i) {
+    String time = dataLines[i];
+  }
+
+  print("result : $result");
+}
